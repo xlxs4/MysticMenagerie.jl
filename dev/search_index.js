56544827324880ed2285da42473f523e909eba93
Documenter.jl <documenter@juliadocs.github.io>
2023-01-05 23:25:19 +0000

    build based on 527a2de

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MysticMenagerie","category":"page"},{"location":"#MysticMenagerie","page":"Home","title":"MysticMenagerie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MysticMenagerie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MysticMenagerie]","category":"page"}]
+}
