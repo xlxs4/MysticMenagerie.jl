aa994a09e7b9af84acec2103a4cb284dc50b10a1
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-09 00:06:02 +0200

    lexer: test underscore identifiers and illegal tokens

--- test/lexer_test.jl
+++ test/lexer_test.jl
@@ -155,3 +155,17 @@ expected = map(x -> m.Token(x...),
 for token in expected
     @test m.next_token!(l) == token
 end
+
+l = m.Lexer("let __snake_case__ = #;")
+expected = map(x -> m.Token(x...),
+               [
+                   (m.LET, "let"),
+                   (m.IDENT, "__snake_case__"),
+                   (m.ASSIGN, "="),
+                   (m.ILLEGAL, "#"),
+                   (m.SEMICOLON, ";"),
+               ])
+
+for token in expected
+    @test m.next_token!(l) == token
+end
