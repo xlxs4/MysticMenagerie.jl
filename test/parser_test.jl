753015b0cdeb172334f8782f02365cc5a27f23e1
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-10 22:30:02 +0200

    test: Add Identifier ExpressionStatement test

--- test/parser_test.jl
+++ test/parser_test.jl
@@ -10,11 +10,22 @@ function check_parser_errors(p::m.Parser)
     end
 end
 
-function test_let_statement(ls::m.LetStatement, name::String)
-    m.token_literal(ls) == "let" || return false
-    m.token_literal(ls.name) == name || return false
-    ls.name.value == name || return false
-    return true
+function test_let_statement(ls::m.Statement, name::String)
+    @assert(isa(ls, m.LetStatement),
+            "Statement is $(typeof(ls)) instead of LetStatement.")
+    @assert(ls.name.value==name,
+            "Statement name value is $(ls.name.value) instead of $name.")
+    @assert(m.token_literal(ls.name)==name,
+            "Statement name token literal is $(m.token_literal(ls.name)) instead of $name.")
+end
+
+function test_identifier_expression(expr::m.Expression, value::String)
+    @assert(isa(expr, m.Identifier),
+            "Expression is $(typeof(expr)) instead of Identifier.")
+    @assert(expr.value==value,
+            "Expression value is $(expr.value) instead of $value.")
+    @assert(m.token_literal(expr)==value,
+            "Expression token literal is $(m.token_literal(expr)) instead of $value.")
 end
 
 @testset "Test parsing LetStatement" begin
@@ -29,16 +40,19 @@ end
 
     @test begin
         check_parser_errors(p)
-        @assert length(program.statements)==3 "Input program does not contain 3 statements. Got $(length(program.statements)) instead."
+        @assert(length(program.statements)==3,
+                "Input program contains $(length(program.statements)) statements instead of 3.")
+
         true
     end
 
     for (i, expected) in enumerate(["x", "y", "foobar"])
         @test begin
-            @assert program.statements[i] isa m.LetStatement
-            "Program statement is $(typeof(program.statements[i])) instead of LetStatement."
-
+            @assert(isa(program.statements[i], m.Statement),
+                    "Program statement is $(typeof(program.statements[i])) instead of Statement.")
             test_let_statement(program.statements[i], expected)
+
+            true
         end
     end
 end
@@ -55,17 +69,36 @@ end
 
     @test begin
         check_parser_errors(p)
-        @assert length(program.statements)==3 "Input program does not contain 3 statements. Got $(length(program.statements)) instead."
+        @assert(length(program.statements)==3,
+                "Input program contains $(length(program.statements)) statements instead of 3.")
+
         true
     end
 
     for i in 1:3
         @test begin
-            @assert program.statements[i] isa m.ReturnStatement
-            "Program statement is $(typeof(program.statements[i])) instead of ReturnStatement."
-
+            @assert(isa(program.statements[i], m.ReturnStatement),
+                    "Program statement is $(typeof(program.statements[i])) instead of ReturnStatement.")
             @assert m.token_literal(program.statements[i]) == "return"
+
             true
         end
+    end
+end
+
+@testset "Test parsing Identifier ExpressionStatement" begin
+    l = m.Lexer("foobar;")
+    p = m.Parser(l)
+    program = m.parse_program!(p)
+
+    @test begin
+        check_parser_errors(p)
+        @assert(length(program.statements)==1,
+                "Input program contains $(length(program.statements)) statements instead of 1.")
+        @assert(isa(program.statements[1], m.ExpressionStatement),
+                "Program statement is $(typeof(program.statements[1])) instead of ExpressionStatement.")
+        test_identifier_expression(program.statements[1].expression, "foobar")
+
+        true
     end
 end
