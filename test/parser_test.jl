00899cbfa87c39688db1587fda655de65d463392
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-12 01:44:41 +0200

    test: Extend operator precedence test with call expressions checks

--- test/parser_test.jl
+++ test/parser_test.jl
@@ -361,6 +361,10 @@ end end
     ("2 / (5 + 5)", "(2 / (5 + 5))"),
     ("-(5 + 5)", "(-(5 + 5))"),
     ("!(true == true)", "(!(true == true))"),
+    ("a + add(b * c) + d", "((a + add((b * c))) + d)"),
+    ("add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+     "add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))"),
+    ("add(a + b + c * d / f + g)", "add((((a + b) + ((c * d) / f)) + g))"),
 ]
     l = m.Lexer(code)
     p = m.Parser(l)
