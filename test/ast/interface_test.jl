a5c8ec35f0bacc59a18d23995e6c898c91d5efb3
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-12 04:58:14 +0200

    test: Interpolate method name directly in AST interface test

--- test/ast/interface_test.jl
+++ test/ast/interface_test.jl
@@ -1,14 +1,14 @@
 @testset "Test implicit interface" begin
-    expected_error(f) = "$(string(f)) is not defined in the concrete type"
+    expected_error(f) = "$f is not defined in the concrete type"
 
     struct DummyNode <: m.Node end
-    @test_throws ErrorException(expected_error(m.token_literal)) m.token_literal(DummyNode())
+    @test_throws ErrorException(expected_error("token_literal")) m.token_literal(DummyNode())
 
     struct DummyExpression <: m.Expression end
-    @test_throws ErrorException(expected_error(m.token_literal)) m.token_literal(DummyExpression())
-    @test_throws ErrorException(expected_error(m.expression_node)) m.expression_node(DummyExpression())
+    @test_throws ErrorException(expected_error("token_literal")) m.token_literal(DummyExpression())
+    @test_throws ErrorException(expected_error("expression_node")) m.expression_node(DummyExpression())
 
     struct DummyStatement <: m.Statement end
-    @test_throws ErrorException(expected_error(m.token_literal)) m.token_literal(DummyStatement())
-    @test_throws ErrorException(expected_error(m.statement_node)) m.statement_node(DummyStatement())
+    @test_throws ErrorException(expected_error("token_literal")) m.token_literal(DummyStatement())
+    @test_throws ErrorException(expected_error("statement_node")) m.statement_node(DummyStatement())
 end
