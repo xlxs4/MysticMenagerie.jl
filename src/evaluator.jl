17cba5e7b6f3b4e2288a3cc6d7a50aecff2a4723
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-13 03:30:14 +0200

    evaluator: evaluator bang operator prefix expressions

--- src/evaluator.jl
+++ src/evaluator.jl
@@ -5,6 +5,10 @@ const _NULL = NullObj()
 function evaluate(node::Node)
     node isa Program && return evaluate_statements(node.statements)
     node isa ExpressionStatement && return evaluate(node.expression)
+    if node isa PrefixExpression
+        right = evaluate(node.right)
+        return evaluate_prefix_expression(node.operator, right)
+    end
     node isa IntegerLiteral && return IntegerObj(node.value)
     node isa BooleanLiteral && return node.value ? _TRUE : _FALSE
     return nothing
@@ -16,4 +20,16 @@ function evaluate_statements(statements::Vector{Statement})
         result = evaluate(stmt)
     end
     return result
+end
+
+function evaluate_prefix_expression(operator::String, right::Object)
+    return operator == "!" ? evaluate_bang_operator_expression(right) : _NULL
+end
+
+function evaluate_bang_operator_expression(right::Object)
+    if right === _FALSE || right === _NULL
+        return _TRUE
+    else
+        return _FALSE
+    end
 end
