2c884c387dba9ba6f9766b47a17c0a66db052e02
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2023-01-15 16:02:31 +0200

    builtin: Add last

--- src/builtins.jl
+++ src/builtins.jl
@@ -32,4 +32,24 @@ const BUILTINS = Dict{String, BuiltinObj}("len" => BuiltinObj(function (args::Ob
                                                                     else
                                                                         return ErrorObj("argument to `first` not supported, got $(type(arg))")
                                                                     end
-                                                                end))
+                                                                end),
+                                          "last" => BuiltinObj(function (args::Object...)
+                                                                   if length(args) != 1
+                                                                       return ErrorObj("wrong number of arguments. got $(length(args)), want 1")
+                                                                   end
+
+                                                                   arg = args[1]
+                                                                   if arg isa StringObj
+                                                                       return length(arg.value) >
+                                                                              0 ?
+                                                                              StringObj(string(last(arg.value))) :
+                                                                              _NULL
+                                                                   elseif arg isa ArrayObj
+                                                                       return length(arg.elements) >
+                                                                              0 ?
+                                                                              last(arg.elements) :
+                                                                              _NULL
+                                                                   else
+                                                                       return ErrorObj("argument to `last` not supported, got $(type(arg))")
+                                                                   end
+                                                               end))
--- test/builtin/last_test.jl
+++ test/builtin/last_test.jl
@@ -1,1 +1,13 @@
+@testset "Test `last` Builtin" begin for (code, expected) in [
+    ("last([1, 2, 3])", 3),
+    ("last([])", nothing),
+    ("last(\"hello\")", "o"),
+    ("last(\"\")", nothing),
+    ("last(1)", "argument to `last` not supported, got INTEGER"),
+    ("last([1, 2], [3, 4])", "wrong number of arguments. got 2, want 1"),
+]
+    evaluated = evaluate_from_code!(code)
+    @test evaluated isa m.Object
 
+    test_object(evaluated, expected)
+end end
